b25a1675133974329e89694bdcecbb45a8376bd5
pmaclin <pcharlesantonio@gmail.com>
2015-02-04 00:20:55 -0600

    Photogram Project
    Dynamically displaying user content, enabling editing and deleting
    functionality.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,42 @@
 class PhotosController < ApplicationController
+
+  def edit_form
+    @photos = Photo.find_by({ :id => params["id"] })
+  end
+
+  def update_row
+    # @photos = Photo.find_by({ :id => params["id"] })
+
+    # @photo.source = params["the_source"]
+    # @photo.caption = params["the_caption"]
+
+    # @photo.save
+  end
+
+  def create_row
+    @photos = Photo.new
+
+    @photos.source = params["the_source"]
+    @photos.caption = params["the_caption"]
+
+    @photos.save
+  end
+
+  def new_form
+  end
+
   def index
     @list_of_photos = Photo.all
+  end
+
+  def show
+    @photos = Photo.find_by({ :id => params["id"] })
+  end
+
+  def destroy
+    @photo = Photo.find_by({ :id => params["id"] })
+
+    @photo.destroy
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,24 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  # Routes to CREATE photos
+  get("/photos/new", { :controller => "photos", :action => "new_form"})
+  get("/create_photo", { :controller => "photos", :action => "create_row"})
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+  # Routes to UPDATE/EDIT photos
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form"})
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row"})
+  # Q: Here the /:id makes that segment dynamic so it accounts for any input from the user. But
+  # since we know that the action attached to the edit on the index page is grabbing the photo.id
+  # from the database, can we not do the same thing with this route (i.e. put in <%= photo.id %> instead of :id?
+
+  # Route to DELETE photos
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy"})
+
+
 
 end
